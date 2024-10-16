bcbecff00f92a7e53c885a7531c4f08af680e0ba
SevenSwen <tihonenko.v.v@gmail.com>
2024-10-16 20:31:33 +0700

    123

--- battleship.js
+++ battleship.js
@@ -148,44 +148,49 @@ class Battleship {
         this.myFleet.forEach(function (ship) {
             console.log();
             console.log(`Please enter the positions for the ${ship.name} (size: ${ship.size})`);
-            for (var i = 1; i < ship.size + 1; i++) {
-                console.log(cliColor.green(`Enter first position of ${ship.size} and direction (i.e A3R):`));
-                const position = readline.question();
+            console.log(cliColor.green(`Enter first position of ${ship.size} and direction (i.e A3R):`));
+            const position = readline.question();
+            if(position[2] === 'R' || position[2] === 'r') {
+                if(position.indexOf(position[0]) + ship.size > 8) {
+                    console.log(cliColor.red("Invalid position, please try again."));
+                    i--;
+                }
 
-                if(position[2] === 'R' || position[2] === 'r') {
-                    for (var j = 0; j < ship.size; j++) {
-                        ship.addPosition(new position(letters.get(position.indexOf(position[0]) + j), position[1]));
-                    }
-                    if(position.indexOf(position[0]) + ship.size > 8) {
-                        console.log(cliColor.red("Invalid position, please try again."));
-                        i--;
-                    }
-                } else if(position[2] === 'D' || position[2] === 'd') {
-                    for (var j = 0; j < ship.size; j++) {
-                        ship.addPosition(new position(position[0], parseInt(position[1]) + j));
-                    }
-                    if(parseInt(position[1]) + ship.size > 8) {
-                        console.log(cliColor.red("Invalid position, please try again."));
-                        i--;
-                    }
-                } else if(position[2] === 'L' || position[2] === 'l') {
-                    for (var j = 0; j < ship.size; j++) {
-                        ship.addPosition(new position(letters.get(position.indexOf(position[0]) - j), position[1]));
-                    }
-                    if(position.indexOf(position[0]) - ship.size < 0) {
-                        console.log(cliColor.red("Invalid position, please try again."));
-                        i--;
-                    }
-                } else if(position[2] === 'U' || position[2] === 'u') {
-                    for (var j = 0; j < ship.size; j++) {
-                        ship.addPosition(new position(position[0], parseInt(position[1]) - j));
-                    }
-                    if(parseInt(position[1]) - ship.size < 0) {
-                        console.log(cliColor.red("Invalid position, please try again."));
-                        i--;
-                    }
+                for (var j = 0; j < ship.size; j++) {
+                    ship.addPosition([letters.get(letters.get(position[0]).value + j).key, position[1]]);
+                }
+            } else if (position[2] === 'D' || position[2] === 'd') {
+                if(parseInt(position[1]) + ship.size > 8) {
+                    console.log(cliColor.red("Invalid position, please try again."));
+                    i--;
+                }
+
+                for (var j = 0; j < ship.size; j++) {
+                    ship.addPosition(position[0], parseInt(position[1]) + j);
+                }
+            } else if (position[2] === 'L' || position[2] === 'l') {
+                if(position.indexOf(position[0]) - ship.size < 0) {
+                    console.log(cliColor.red("Invalid position, please try again."));
+                    i--;
+                }
+
+                for (var j = 0; j < ship.size; j++) {
+                    ship.addPosition([letters.get(letters.get(position[0]).value - j).key, position[1]]);
+                }
+
+            } else if (position[2] === 'U' || position[2] === 'u') {
+                if(parseInt(position[1]) - ship.size < 0) {
+                    console.log(cliColor.red("Invalid position, please try again."));
+                    i--;
+                }
+
+                for (var j = 0; j < ship.size; j++) {
+                    ship.addPosition(position[0], parseInt(position[1]) + j);
                 }
             }
+            // if (this.checkCollision(ship, this.myFleet)) {
+            //     console.log(cliColor.red("Invalid position (checkCollision), please try again."));
+            // }
         });
     }
 
@@ -210,6 +215,19 @@ class Battleship {
             console.error("Error reading file:", err);
         }
         this.writeToFile();
+    }
+
+    checkCollision(ship, fleet) {
+        for (let index = 0; index < fleet.length; index++) {
+            for (let pos = 0; pos < fleet[index].positions.length; pos++) {
+                for (let i = 0; i < ship.positions.length; i++) {
+                    if (fleet[index].positions[pos].column === ship.positions[i].column && fleet[index].positions[pos].row === ship.positions[i].row) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     writeToFile() {
